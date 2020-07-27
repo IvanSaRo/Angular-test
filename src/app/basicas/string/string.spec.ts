eb93259165505f5f64f7fec43c249bf58c986191
Iván Sánchez <ivansancrodr@gmail.com>
2020-07-28 00:17:32 +0200

    Uso de toBe y uso de toContain

--- src/app/basicas/string/string.spec.ts
+++ src/app/basicas/string/string.spec.ts
@@ -8,6 +8,18 @@ describe( 'Pruebas de strings', () => {
         const  resp = mensaje("Iván");
 
         expect( typeof resp ).toBe('string');
+        // Espera que sea un string
+
+
+    })
+
+    it( 'Debe devolver un saludo con el nombre enviado', () => {
+
+        const nombre = "Pedro"
+        const  resp = mensaje( nombre );
+
+        expect( resp ).toContain( nombre );
+        // Espera que lo que se devuelva contenga la const nombre
 
 
     })
