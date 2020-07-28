f191e78978fff4486a9980d7994ac24a1b17913d
Iván Sánchez <ivansancrodr@gmail.com>
2020-07-28 16:05:33 +0200

    funciones before y after vistas, ng test --code-coverage para ver cobertura de test usado

--- src/app/class/class.spec.ts
+++ src/app/class/class.spec.ts
@@ -1,10 +1,28 @@
 import { Player } from './class';
 
-describe("Pruebas de clase", () => {
 
+
+
+
+
+
+describe("Pruebas de clase", () => {
+    let player = new Player
+    //beforeAll( );
+    beforeEach( () => {
+        player = new Player;
+    });
+
+    //afterAll();
+    /* afterEach(  () => {
+        player.hp = 100;
+    }); */
+    
+    
+    
     it(" Si recibe 20 de daño ha de devolver 80hp", () => {
 
-        const player = new Player;
+        //const player = new Player;
         const res = player.getHurt(20);
 
         expect( res ).toBe(80);
@@ -13,10 +31,19 @@ describe("Pruebas de clase", () => {
 
     it(" Si recibe 50 de daño ha de devolver 50hp", () => {
 
-        const player = new Player;
+       // const player = new Player;
         const res = player.getHurt(50);
 
         expect( res ).toBe(50);
 
     })
+
+    it(" Si recibe 100 de daño o mas ha de devolver 0hp", () => {
+
+        // const player = new Player;
+         const res = player.getHurt(120);
+ 
+         expect( res ).toBe(0);
+ 
+     })
 })
